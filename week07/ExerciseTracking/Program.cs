98684a31e2968da494a2021b9e8f5f5d25348496
ja-ribeiro <juli.ribeiro@hotmail.com>
2026-02-16 17:31:51 -0300

    Finished Project

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -5,5 +5,21 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        
+        List<Activity> activities = new List<Activity>();
+
+        Running running = new Running("03 Jan 2026", 30, 5.0);
+        Cycling cycling = new Cycling("04 Jan 2026", 40, 20.0);
+        Swimming swimming = new Swimming("05 Jan 2026", 35, 20);
+
+        activities.Add(running);
+        activities.Add(cycling);
+        activities.Add(swimming);
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
+    
 }
